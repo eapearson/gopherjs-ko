721983e9ded7982aea6ed55bb7cc55ed73e7de11
Michael Bitzi <michael.bitzi@futuretek.ch>
2015-05-08 10:18:21 +0200

    fix gh-pages

--- example/js/router.go
+++ example/js/router.go
@@ -29,7 +29,7 @@ func (r *Router) init() {
 	// We use pagejs to do our routing.
 	page := js.Global.Get("page")
 
-	page.Call("base", "/gopherjs-ko/example/static")
+	page.Call("base", "/gopherjs-ko/example/static/")
 
 	// Handle the home page
 	page.Invoke("/", func(ctx *js.Object) {
